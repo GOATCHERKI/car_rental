af1814670227b2e752bc4923cf6d2922a2248a25
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-11 20:09:56 +0300

    first commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FeatureSection from '../components/FeatureSection'
+import Banner from '../components/Banner'
+import Reviews from '../components/Reviews'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeatureSection/>
+      <Banner/>
+      <Reviews/>
+    </>
+  )
+}
+
+export default Home
