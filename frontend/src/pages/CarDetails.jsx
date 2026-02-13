3ca4bf2deee6cc31eaf4b14560fd3469e7ed0f0b
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-13 11:07:03 +0300

    Cars page

--- frontend/src/pages/CarDetails.jsx
+++ frontend/src/pages/CarDetails.jsx
@@ -10,6 +10,10 @@ const CarDetails = () => {
   const car = useMemo(() => dummyCarData.find((item) => item._id === id), [id])
   const currency = import.meta.env.VITE_CURRENCY
 
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+  }
+
   return car ?(
     <div className='px-6 md:px-16 lg:px-24 xl:px-32 mt-16'>
       <button onClick={()=> navigate(-1)} className='flex items-center gap-2 mb-6 text-gray-500 cursor-pointer'>
@@ -63,7 +67,7 @@ const CarDetails = () => {
           </div>
         </div>
         
-        <form className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+        <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
                 <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>
                   {currency}{car.pricePerDay}<span className='text-base text-gray-400 font-normal'> per day</span>
                 </p>
