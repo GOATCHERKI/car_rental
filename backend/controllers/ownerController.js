89b568e42da8a1828a8af01e6f951728c4c4e0aa
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-18 10:03:26 +0300

    imagekit implementation, add car method with image upload and owner routes

--- backend/controllers/ownerController.js
+++ backend/controllers/ownerController.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/Car.js";
 import User from "../models/User.js";
+import fs from "fs";
 
 export const changeRoleToOwner = async (req, res) => {
   try {
@@ -16,6 +19,34 @@ export const addCar = async (req, res) => {
     const { _id } = req.user;
     let car = JSON.parse(req.body.carData);
     const imageFile = req.file;
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/cars",
+    });
+
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transform: [{ width: "1280" }, { quality: "auto" }, { format: "webp" }],
+    });
+
+    const image = optimizedImageUrl;
+    await Car.create({ ...car, owner: _id, image });
+
+    res.json({ succes: true, message: "Car added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getOwnerCars = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const cars = await Car.find({ owner: _id });
+    res.json({ succes: true, cars });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
