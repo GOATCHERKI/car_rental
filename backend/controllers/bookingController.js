c11b7e7a10ac46198c90bc0fe4ca4f0e423680c2
GOATCHERKI <c.zikocherki@gmail.com>
2026-02-20 09:45:32 +0300

    booking controller and routes

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -68,3 +68,55 @@ export const createBooking = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+export const getUserBookings = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const bookings = await Booking.find({ user: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getOwnerBookings = async (req, res) => {
+  try {
+    if (req.user.role !== "owner") {
+      return res.json({ success: false, message: "Unauthorized" });
+    }
+    const bookings = await Booking.find({ owner: req.user._id })
+      .populate("car user")
+      .select("-user.password")
+      .sort({ createdAt: -1 });
+
+    res.json({ success: true, bookings });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const changeBookingStatus = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { bookingId, status } = req.body;
+
+    const booking = await Booking.findById(bookingId);
+
+    if (booking.owner.toString() !== _id.toString()) {
+      return es.json({ success: false, message: "Unauthorized" });
+    }
+
+    booking.status = status;
+    await booking.save();
+
+    res.json({ success: true, message: "Booking status updated" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
